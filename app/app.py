d88072be12df253510ba4b86ea46165ba1f6c521
petemcnie <petemcnie0@gmail.com>
2022-10-13 20:40:29 +1300

    Refactor route name, remove example file

--- app/app.py
+++ app/app.py
@@ -6,14 +6,12 @@ from app.blueprints.test import test
 
 app = Flask(__name__)
 app.config.from_object("app.settings.default")
-# app.config.from_envvar("FLASK_APP")
 app.config.from_envvar("APP_SETTINGS")
 
 # Set up a direnv file in backend to automatcially set environment variables
 
 m.db.init_app(app)
 
-app.register_blueprint(example)
 app.register_blueprint(test)
 
 # app.register_blueprint(example) Keep adding each blueprint file here
