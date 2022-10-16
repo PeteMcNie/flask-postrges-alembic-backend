966e79f29cda244d6e535a62a45557a24e46cc2f
petemcnie <petemcnie0@gmail.com>
2022-10-16 21:30:11 +1300

    Tidy up test blueprints file so that params can be passed in via the url and not command line

--- app/blueprints/test.py
+++ app/blueprints/test.py
@@ -9,60 +9,58 @@ test = Blueprint("test", __name__, url_prefix='/test')
 
 @test.route("/test-get-all", methods=["GET"])
 def test_get_all():
-	test_data = m.TestTable.query.all()
+	test_data = m.TestTable.query.order_by(m.TestTable.count.asc()).all()
 	return jsonify([data.__repr__() for data in test_data])
 
 
-@test.route("/test-get-one/<number>", methods=["GET"])
-def test_get_one(number):
-	row = m.TestTable.query.filter(m.TestTable.count == number).one_or_none()
+@test.route("/test-get-one/<count>", methods=["GET"])
+def test_get_one(count):
+	row = m.TestTable.query.filter(m.TestTable.count == count).one_or_none()
 	if row is None:
-		flash(f"A table with row count number {number} was not found!")
+		flash(f"A table with row count {count} was not found!")
 		return redirect(url_for('test_get_all')) 
 	return jsonify(row.__repr__())
 
 
-@test.route("/test-add", methods=["POST"])
-def add_row():
-	if not request.json:
-		abort(400)
+@test.route("/test-add/<name>/<count>", methods=["GET", "POST"])
+def add_row(name, count):
 	row = m.TestTable(
 		created=datetime.utcnow(),
-		name=request.json.get('name'),
-		count=request.json.get('count'),
+		name=name if name else None,
+		count=count if count else 0,
 	)
 	m.db.session.add(row)
 	m.db.session.commit()
 	flash(f"New row successfully added")
-	return redirect(url_for('test_get_all'))
+	return redirect(url_for('test.test_get_all'))
 
 
-@test.route("/test-update/<number>", methods=["GET", 'POST'])
-def update_row(number):
-	if not request.json:
-		abort(400)
-
-	row = m.TestTable.query.filter(m.TestTable.count == number).one_or_none()
+@test.route("/test-update/<id>/<name>/<count>", methods=["GET", 'POST'])
+def update_row(id, name, count):
+	row = m.TestTable.query.filter(m.TestTable.id == id).one_or_none()
 
 	if row is None:
-		flash(f"A table with row count number {number} was not found!")
-		return redirect(url_for('test_get_all'))
+		flash(f"A table with row id {id} was not found!")
+		return redirect(url_for('test.test_get_all'))
 
-	row.name = request.json.get('name', row.name)
-	row.count = request.json.get('count', row.count)
+	if name:
+		row.name = name
+	if count:
+		row.count = count
 
 	m.db.session.commit()
 
-	return redirect(url_for(f'test_get_all'))
+	flash(f"Test table with row id {id} was updated")
+	return redirect(url_for(f'test.test_get_all'))
 
 
-@test.route("/test-delete/<number>", methods=["GET", "POST"])
-def delete_row(number):
-	row = m.TestTable.query.filter(m.TestTable.count == number).one_or_none()
+@test.route("/test-delete/<id>", methods=["GET", "POST"])
+def delete_row(id):
+	row = m.TestTable.query.filter(m.TestTable.id == id).one_or_none()
 	if row is None:
-		flash(f"A table row with count number {number} does not exist!")
-		return redirect(url_for('test_get_all'))
+		flash(f"A table row with id {id} does not exist!")
+		return redirect(url_for('test.test_get_all'))
 	m.db.session.delete(row)
 	m.db.session.commit()
-	flash(f"A table with row count number {number} was deleted!")
-	return redirect(url_for('test_get_all'))
+	flash(f"A table with row id {id} was deleted!")
+	return redirect(url_for('test.test_get_all'))
