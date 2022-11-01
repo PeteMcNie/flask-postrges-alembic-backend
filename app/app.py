0bcc5697c354a7dbd0cb4df53c3e567682cd712d
petemcnie <petemcnie0@gmail.com>
2022-11-01 14:16:51 +1300

    Add flask cors

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 import model as m
 from app.blueprints.test import test
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object("app.settings.default")
 app.config.from_envvar("APP_SETTINGS")
 
